9dfa91eb928444cc8ae5ad3174f06d271a877f11
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2023-03-16 15:15:06 -0300

    aula

---
+++ pythonexercicios/pycharmprojeto/pythonteste/aula19.py
@@ -0,0 +1,10 @@
+valores = list()
+valores.append(5)
+valores.append(9)
+valores.append(4)
+valores.append(3)
+for c, v in enumerate(valores):
+    print(f'posição {c} encontrei o valores {v}!')
+print('cheguei ao final da lista.')
+
+
