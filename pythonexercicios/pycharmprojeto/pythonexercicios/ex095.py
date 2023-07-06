129e5e6d169c6b749e038cad96a3e7621f1af1a6
Lucas Ferreira Silva <silvaflucas88@gmail.com>
2023-07-06 11:52:12 -0300

    ex095.py
    Aprimorando os dicionários.

---
+++ pythonexercicios/pycharmprojeto/pythonexercicios/ex095.py
@@ -0,0 +1,48 @@
+time = list()
+jogador = dict()
+partidas = list()
+
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input(' Nome do jogador:'))
+    tot = int(input(f'quantas partidas {jogador["nome"]} jogou?'))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'  quantas gols na partida {c+1}ª?')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+         resp = str(input('quer continuar? [S/N]')).upper()[0]
+         if resp in 'SN':
+             break
+         print('ERRO resposta apenas S ou N. ')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+    print()
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para prar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f' ERRO! Não exite jogador com código {busca}!')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca] ["nome"]}:')
+        for i, g in enumerate(time [busca]['gols']):
+            print(f'    No jogo {i} fex {g} gols.')
+    print('-' * 40)
+print('<< VOLTE SEMPRE>>')
+
+
+
